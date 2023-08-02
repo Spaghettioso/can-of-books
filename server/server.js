625ae8ad2d7639d92933c1fb5ea48c0bdea12efe
Johnny <jonathanoreilly@live.co.uk>
2023-08-02 15:21:39 +0100

    its alive

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bp = require('body-parser')
+const bp = require('body-parser');
 require('dotenv').config();
 
 const PORT = process.env.port || 8090;
@@ -9,6 +9,9 @@ const PORT = process.env.port || 8090;
 const app = express();
 app.use(cors());
 app.use(bp.json());
+
+const Book = require("./models/book")
+mongoose.connect(process.env.DATABASE_URL)
 
 app.get("/", (request, response)=>{
     response.status(200).json("howdy yall")
