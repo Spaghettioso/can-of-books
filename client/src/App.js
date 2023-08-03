56f389d817c56ce621e91644130b6ae24d1e9f39
Johnny <jonathanoreilly@live.co.uk>
2023-08-03 10:15:05 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@
 import './reset.css';
 import './App.css';
 import Main from './components/Main';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,8 +1,24 @@
 import React, {useEffect,useState} from 'react'
 import axios from 'axios'
 
+
 export default function Main() {
-  return (
-    <div>Main</div>
-  )
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooks()
+  },[])
+
+  async function getBooks(){
+    let API = 'http://localhost:8090/books'
+    const result = await axios.get(API)
+    console.log(result.data)
+    setBooks(result.data)
+  }
+
+
+
+
 }
+
+
