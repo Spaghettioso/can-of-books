8f9d18cc4da16e638c09e5ecd0ddbe07e54e3f6b
Johnny <jonathanoreilly@live.co.uk>
2023-08-03 11:41:40 +0100

    update

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,6 +9,7 @@ export default function Main() {
     getBooks()
   },[])
 
+  //READ
   async function getBooks(){
     let API = 'http://localhost:8090/books'
     const result = await axios.get(API)
@@ -16,9 +17,33 @@ export default function Main() {
     setBooks(result.data)
   }
 
+  //CREATE
+  const handleAddBook = async newBookFormData => {
+    const res = await axios.post('http://localhost:8090/books', newBookFormData)
+    setBooks([...books, res.data])
+  }
 
+  //DELETE
 
+  const handleDelete = async (id) => {
+    const res = await axios.delete(`https://localhost:8090/books/${id}`)
+    getBooks()
+  }
 
+  //UPDATE
+
+  const handleUpdateBook = async (book) => {
+    await axios.put(`https://localhost:8090/books/${book_id}`, book)
+    getBooks();
+  } 
+
+  return(
+    <div>
+      <Form onSubmitFunc={handleAddBook} />
+      <h3>This is my Main!</h3>
+      <BookCard books={books} handleDelete={handleDelete} handleUpdateBook={handleUpdateBook} />
+    </div>
+  )
 }
 
 
